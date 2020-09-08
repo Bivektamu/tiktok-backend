49e75453c53f9ff43ca985a5c4dc02f766b00b7f
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-09 07:48:09 +1000

    Initialize backend of tiktok

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => res.status(200).send('hello world'));
+
+app.listen(port, () => console.log(`listening on localhost:${port}`));
